785f0e0ebfbbee2f2769279c73d07b20d07b5fda
maxim.hunko <maxim.hunko@developex.net>
2022-05-18 12:27:08 +0300

    Refactored project structure.

--- Src/main.c
+++ Src/main.c
@@ -32,25 +32,28 @@ int main(void)
 static void clockConfig(void)
 {
     RCC->CR   |=  RCC_CR_HSEON;
-    while((RCC->CR & RCC_CR_HSERDY)==0) {}
+    while ((RCC->CR & RCC_CR_HSERDY)==0) {
+    }
     RCC->CR   |=  RCC_CR_CSSON;
 
     RCC->CFGR &= ~RCC_CFGR_PLLXTPRE;
 
-    FLASH->ACR|=  FLASH_ACR_PRFTBE;
-    FLASH->ACR&= ~FLASH_ACR_LATENCY;
-    FLASH->ACR |= FLASH_ACR_LATENCY_2;
+    FLASH->ACR |=  FLASH_ACR_PRFTBE;
+    FLASH->ACR &= ~FLASH_ACR_LATENCY;
+    FLASH->ACR |=  FLASH_ACR_LATENCY_2;
 
     RCC->CFGR  |= RCC_CFGR_PLLSRC;
     RCC->CR   &= ~RCC_CR_PLLON;
     RCC->CFGR &= ~RCC_CFGR_PLLMULL;
     RCC->CFGR |=  RCC_CFGR_PLLMULL9;
     RCC->CR   |=  RCC_CR_PLLON;
-    while((RCC->CR & RCC_CR_PLLRDY) == 0) {}
+    while ((RCC->CR & RCC_CR_PLLRDY) == 0) {
+    }
 
     RCC->CFGR &= ~RCC_CFGR_SW;
     RCC->CFGR |=  RCC_CFGR_SW_PLL;
-    while((RCC->CFGR&RCC_CFGR_SWS)!= 0x08) {}
+    while ((RCC->CFGR&RCC_CFGR_SWS)!= 0x08) {
+    }
 
     RCC->CFGR &= ~RCC_CFGR_PPRE1;
     RCC->CFGR |=  RCC_CFGR_PPRE1_DIV2;
@@ -105,7 +108,7 @@ static void loadApplication(void)
     // This block of code sets the vector table location and offset.
     // Here we specify if the vector table is in RAM or FLASH memory. 
     // This parameter can be 0x08000000
-    // app_image_addr is vector Table base offset field. (MAIN_PROGRAM_START_ADDRESS) 
+    // kJumpAddress is vector Table base offset field. (MAIN_PROGRAM_START_ADDRESS) 
     // This value must be a multiple of 0x100.
     SCB->VTOR = (((uint32_t)0x08000000) | (MAIN_PROGRAM_START_ADDRESS & (uint32_t)0x1FFFFF80));
 
@@ -113,8 +116,10 @@ static void loadApplication(void)
 
     gApplicationFunction = (pFunction_t)kJumpAddress;
 
+    // Set main stack pointer register
     __set_MSP(*(__IO uint32_t*)MAIN_PROGRAM_START_ADDRESS);
 
-    // In Application you nead enable global interrupt
+    // Jump to main application
+    // NOTE: In Application you nead enable global interrupt
     gApplicationFunction();
 }
