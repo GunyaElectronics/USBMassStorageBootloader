a048d016dc6d268636c2cbacc95ebc2fef760893
maxim.hunko <maxim.hunko@developex.net>
2022-05-18 16:05:23 +0300

    Fixed flash size for selected MCU.

--- Inc/flash.h
+++ Inc/flash.h
@@ -2,7 +2,15 @@
 #define _FLASH_
 
 #define USER_FLASH_START_ADDRESS    0x08002C00
-#define FLASH_END_ADDRESS           0x08010000
+
+#if   defined(STM32F103x8)
+#   define FLASH_END_ADDRESS        0x08010000
+#elif defined(STM32F103xB)
+#   define FLASH_END_ADDRESS        0x08020000
+#else
+#   error "Set MCU part number"
+#endif
+
 #define PAGE_SIZE                   1024
 
 uint8_t isFlashReady(void);
